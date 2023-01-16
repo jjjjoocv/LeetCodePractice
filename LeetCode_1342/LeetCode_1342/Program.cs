e67cb247951de35c1953d192dc430981f020f744
FEATHER_PC\hungy <hungyufeather@gmail.com>
2023-01-16 15:16:50 +0800

    https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/

---
+++ LeetCode_1342/LeetCode_1342/Program.cs
@@ -0,0 +1,32 @@
+﻿//20230116 by hungyufeather@gmail.com
+using System;
+
+namespace LeetCode_1342
+{
+    public class Solution
+    {
+        public int NumberOfSteps(int num)
+        {
+            int step = 0;
+            while (num != 0)
+            {
+                if (num % 2 == 0) num /= 2;
+                else num -= 1;
+                step++;
+            }
+            return step;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 25;
+            Solution solution = new Solution();
+            Console.WriteLine(solution.NumberOfSteps(num));
+
+            Console.ReadKey();
+        }
+    }
+}
