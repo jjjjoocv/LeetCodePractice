366a52820033e5c079c4ed73478e74da8d268f08
FEATHER_PC\hungy <hungyufeather@gmail.com>
2023-01-16 17:16:37 +0800

    https://leetcode.com/problems/assign-cookies/description/

---
+++ LeetCode_0455/LeetCode_0455/Program.cs
@@ -0,0 +1,34 @@
+﻿//20230116 by hungyufeather@gmail.com
+using System;
+
+namespace LeetCode_0455
+{
+    public class Solution
+    {
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            Array.Sort(g);
+            Array.Sort(s);
+            int child = 0;
+            int cookie = 0;
+            while (child < g.Length && cookie < s.Length)
+            {
+                if (g[child] <= s[cookie]) child++;
+                cookie++;
+            }
+            return child;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] children = new int[] { 1, 3 };
+            int[] cookies = new int[] { 1, 2, 5 };
+            Solution solution = new Solution();
+            Console.WriteLine(solution.FindContentChildren(children, cookies));
+            Console.ReadKey();
+        }
+    }
+}
