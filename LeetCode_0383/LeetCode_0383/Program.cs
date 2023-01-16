63a319627b651fbc1e7c4351ac1683ae44ba28a6
FEATHER_PC\hungy <hungyufeather@gmail.com>
2023-01-16 16:01:34 +0800

    https://leetcode.com/problems/ransom-note/description/

---
+++ LeetCode_0383/LeetCode_0383/Program.cs
@@ -0,0 +1,32 @@
+﻿//20230116 by hungyufeather@gmail.com
+using System;
+
+namespace LeetCode_0383
+{
+    public class Solution
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            if (ransomNote.Length > magazine.Length) return false;
+            for (int i = 0; i < ransomNote.Length; i++)
+            {
+                int str = magazine.IndexOf(ransomNote[i]);
+                if (str == -1) return false;
+                magazine = magazine.Remove(str, 1);
+            }
+            return true;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ransomNote = "ab";
+            string magazine = "bba";
+            Solution solution = new Solution();
+            Console.WriteLine(solution.CanConstruct(ransomNote, magazine));
+            Console.ReadKey();
+        }
+    }
+}
