27f2f7f39d804939fa243ef09515fe925154a17c
FEATHER_PC\hungy <hungyufeather@gmail.com>
2023-01-16 12:25:09 +0800

    https://leetcode.com/problems/running-sum-of-1d-array/description/

---
+++ LeetCode_1480/LeetCode_1480/Program.cs
@@ -0,0 +1,30 @@
+﻿//20230116 by hungyufeather@gmail.com
+using System;
+using System.Linq;
+
+namespace LeetCode_1480
+{
+    public class Solution
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+            return nums;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[]{ 1, 2, 3, 4 };
+            Solution solution = new Solution();
+            solution.RunningSum(input).ToList().ForEach(Console.WriteLine);
+
+            Console.ReadKey();
+        }
+    }
+}
