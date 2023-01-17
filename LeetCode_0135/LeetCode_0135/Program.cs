804d3ef03b1e30d90153fa98e7929de1f503946f
FEATHER_PC\hungy <hungyufeather@gmail.com>
2023-01-17 17:55:11 +0800

    https://leetcode.com/problems/candy/description/

---
+++ LeetCode_0135/LeetCode_0135/Program.cs
@@ -0,0 +1,38 @@
+﻿//20230117 by hungyufeather@gmail.com
+using System;
+using System.Linq;
+
+namespace LeetCode_0135
+{
+    public class Solution
+    {
+        public int Candy(int[] ratings)
+        {
+            int count = ratings.Length;
+            if (count == 1) return count;
+            int[] candy = new int[count];
+            Array.Fill(candy, 1);
+
+            for (int i = 1; i < count; i++)
+            {
+                if (ratings[i - 1] < ratings[i]) candy[i] = candy[i-1] + 1;
+            }
+            for (int i = count - 1; i > 0; i--)
+            {
+                if (ratings[i - 1] > ratings[i]) candy[i - 1] = Math.Max(candy[i - 1], candy[i] + 1);
+            }
+            return candy.Sum();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[] { 1, 0, 2 };
+            Solution solution = new Solution();
+            Console.WriteLine(solution.Candy(input));
+            Console.ReadKey();
+        }
+    }
+}
