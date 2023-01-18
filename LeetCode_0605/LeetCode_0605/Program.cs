a07e4feee7fae602c59b7fec718fec3745c11f00
FEATHER_PC\hungy <hungyufeather@gmail.com>
2023-01-19 00:29:05 +0800

    https://leetcode.com/problems/can-place-flowers/description/

---
+++ LeetCode_0605/LeetCode_0605/Program.cs
@@ -0,0 +1,35 @@
+﻿//20230118 by hungyufeather@gmail.com
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode_0605
+{
+    public class Solution
+    {
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if ((i == 0 || flowerbed[i - 1] == 0) && flowerbed[i] == 0 && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+                {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+                if (n <= 0) return true;
+            }
+            return false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            int[] input = new int[] { 1, 0, 0, 0, 1 };
+            Solution solution = new Solution();
+            Console.WriteLine(solution.CanPlaceFlowers(input, num));
+            Console.ReadKey();
+        }
+    }
+}
