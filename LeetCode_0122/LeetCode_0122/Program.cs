8b1eebd747ff864c8a36e4dbc6e945a4f2b57764
FEATHER_PC\hungy <hungyufeather@gmail.com>
2023-01-20 23:51:12 +0800

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/

---
+++ LeetCode_0122/LeetCode_0122/Program.cs
@@ -0,0 +1,32 @@
+﻿//20230120 by hungyufeather@gmail.com
+using System;
+
+namespace LeetCode_0122
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                if (prices[i] < prices[i + 1])
+                {
+                    maxProfit += prices[i + 1] - prices[i];
+                }
+            }
+            return maxProfit;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[] { 7, 1, 5, 3, 6, 4 };
+            Solution solution = new Solution();
+            Console.WriteLine(solution.MaxProfit(input));
+            Console.ReadKey();
+        }
+    }
+}
