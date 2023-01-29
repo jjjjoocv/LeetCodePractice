cd6ff6da678a77e4cb469e10bb0345f19b08ba79
FEATHER_PC\hungy <hungyufeather@gmail.com>
2023-01-29 22:54:15 +0800

    https://leetcode.com/problems/sqrtx/description/

---
+++ LeetCode_0069/LeetCode_0069/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeetCode_0069
+{
+    public class Solution
+    {
+        public int MySqrt(int x)
+        {
+            if (x<=1) return x;
+            int left = 1;
+            int right = x;
+            while(left<=right)
+            {
+                int mid = right + (left - right) / 2;
+                int sqrt = x / mid;
+                if (sqrt == mid) return mid;
+                else if (sqrt < mid) right = mid - 1;
+                else left = mid + 1;
+            }
+            return right;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 8;
+            Solution solution = new Solution();
+            Console.WriteLine(solution.MySqrt(input));
+            Console.ReadKey();
+        }
+    }
+}
