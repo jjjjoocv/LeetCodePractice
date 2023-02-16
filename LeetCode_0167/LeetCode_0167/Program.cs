c0a1bddeb0be50a3a777c4eb0324babdd3b818f3
FEATHER_PC\hungy <hungyufeather@gmail.com>
2023-02-16 23:52:23 +0800

    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/

---
+++ LeetCode_0167/LeetCode_0167/Program.cs
@@ -0,0 +1,33 @@
+﻿//20230216 by hungyufeather@gmail.com
+using System;
+
+namespace LeetCode_0167
+{
+    public class Solution
+    {
+        public int[] IndexArray (int[] numbers, int target)
+        {
+            int left = 0;
+            int right = numbers.Length - 1;
+            while (left < right)
+            {
+                if (numbers[left] + numbers[right] == target) break;
+                else if (numbers[left] + numbers[right] > target) right--;
+                else left++;
+            }
+            return new int[2] { left + 1, right + 1 };
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[4] { 2, 7, 11, 15 };
+            int target = 9;
+            Solution solution = new Solution();
+            Console.WriteLine($"[{solution.IndexArray(input,target)[0]},{solution.IndexArray(input, target)[1]}]");
+            Console.ReadKey();
+        }
+    }
+}
